d6e95962583c78981899198f27653ad5529c342f
ajith <ajithraveendranr@gmail.com>
2022-07-06 12:03:10 +0530

    display success in a para

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import {screen} from '@testing-library/react'
 
 function Form(props) {
 
     const obj = {
         name:/\w/i,
         email:/^\w+@/,
-        mobile:/\d/,
+        mobile:/^\d+$/,
         password:/\w{6}/,
-        address:/\w{4}/
+        // address:/\w{4}/
     }
 
     const callSubmit = (e) => {
         e.preventDefault();
+        
+        const para = document.querySelector('#para');
         const name = document.querySelector('#name').value;
         const email = document.querySelector('#email').value;
         const phNumber = document.querySelector('#phoneNumber').value;
@@ -19,35 +22,34 @@ function Form(props) {
         const gender = document.querySelector('#gender').value;
        
   if(name == '' && email == '' || phNumber == '' || password == ''){
-        alert('All fields are mandatory');
+        window.alert('All fields are mandatory');
         return;
   }
 
        if(!obj.name.test(name)){
-        alert('Name is not alphanumeric');
+        window.alert('Name is not alphanumeric');
         return;
        }
        if(!obj.email.test(email)){
-        alert('Email must contain @');
+        window.alert('Email must contain @');
         return;
        }
            if(gender != 'male' && gender != 'female' && gender !='other' ){
-            alert('Please identify as male, female or others');
+            window.alert('Please identify as male, female or others');
             return;
            }
 
            if(!obj.mobile.test(phNumber)){
-            alert('Phone Number must contain only numbers');
+            window.alert('Phone Number must contain only numbers');
             return;
            }
 
            if(!obj.password.test(password)){
-            alert('Password must contain atleast 6 letters');
+            window.alert('Password must contain atleast 6 letters');
             return;
            }
 let uName = email.split('@')[0];
-
-alert('Hello' + " " + uName);
+para.innerHTML = `Hello ${uName}`;
 
         
     }
@@ -80,6 +82,7 @@ alert('Hello' + " " + uName);
 
             <button data-testid = 'submit' type='submit'>Submit</button>
             </form>
+            <p id="para"></p>
         </div>
     );
 }
